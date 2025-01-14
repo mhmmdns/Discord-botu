27d431f028ac7456e9934c20ac20163b354101b4
Muhammed Enes Taşkesen <110444084+mhmmdns@users.noreply.github.com>
2025-01-14 20:19:23 +0300

    Create sifre_olusturan_bot.py

---
+++ sifre_olusturan_bot.py
@@ -0,0 +1,28 @@
+import discord
+from bot_mantik import gen_pass
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$sifre_ver'):
+        await message.channel.send("Şifreniz: " + gen_pass(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run("Buraya botunuzun Token bilgisini yazmanız gerekiyor.")
